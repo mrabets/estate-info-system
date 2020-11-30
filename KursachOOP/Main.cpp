f9d97cf817e781703b8664e4f42c0a09afe751cc
Karim Marabet <newrabgo@gmail.com>
2020-11-30 18:52:22 +0300

    clear

--- KursachOOP/Main.cpp
+++ KursachOOP/Main.cpp
@@ -7,7 +7,19 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	
-	NewBuilding nb(1, 4, 5, 22.33f, 1999, "Minsk", "Karim", 22.5f);
+	const int SIZE = 3;
+
+	NewBuilding newBuildings[SIZE] =
+	{
+		NewBuilding(1, 4, 5, 22.33f, 1999, "Minsk", "Karim Marabet", 62.1f),
+		NewBuilding(3, 2, 3, 65.21f, 2100, "Moscow", "Olga Boreyko", 42.5f),
+		NewBuilding(1, 4, 5, 22.33f, 1999, "Minsk", "Alex Dmitriev", 33.2f),
+	};
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << newBuildings[i];
+	}
 
 	return 0;
 }
